11b44410ebd391162e1d6c60881cb04fbc1c68be
Marcos <marcos.goncalves@CPACONSULTING.COM.BR>
2018-04-16 14:53:40 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/gerenciador/web/NovaEmpresa.java
@@ -0,0 +1,33 @@
+package br.com.alura.gerenciador.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.dao.EmpresaDAO;
+import br.com.alura.gerenciador.model.Empresa;
+
+@WebServlet(urlPatterns ="/novaEmpresa")
+public class NovaEmpresa extends HttpServlet{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String nome = req.getParameter("nome");
+		
+		Empresa empresa = new Empresa(nome);
+		
+		new EmpresaDAO().adiciona(empresa);
+		
+		PrintWriter writer = resp.getWriter();
+		
+		writer.println("<html><body> Empresa " + empresa.getNome() + "adiciona!</body></html>");
+	}
+}
