658513c53d273cfd7116856f11b1d1bc9a8f8ef4
Marcos <marcos.goncalvesdossantos@gmail.com>
2018-04-20 09:22:43 -0300

    Atualização

--- src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
+++ src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -8,20 +8,42 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
-@WebFilter(urlPatterns="/*")
+@WebFilter(urlPatterns = "/*")
 public class FiltroDeAuditoria implements Filter {
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
+
+		Cookie cookie = getUsuario(req);
+		String usuario = "deslogado";
 		
-		System.out.println("Acessando a URI " + req.getRequestURI());
+		if (cookie != null) {
+			usuario = cookie.getValue();
+		}
+		
+		System.out.println("Usuario " + usuario + " acessando a URI " + req.getRequestURI());
 		
 		chain.doFilter(request, response);
+
 	}
 
+	private Cookie getUsuario(HttpServletRequest req) {
+		Cookie[] cookies = req.getCookies();
+
+		if (cookies == null)
+			return null;
+		
+		for (Cookie cookie : cookies) {
+			if (cookie.getName().equals("usuario")) {
+				return cookie;
+			}
+		}
+		return null;
+	}
 
 }
--- src/main/java/br/com/alura/gerenciador/web/Login.java
+++ src/main/java/br/com/alura/gerenciador/web/Login.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import br.com.alura.gerenciador.dao.UsuarioDAO;
 import br.com.alura.gerenciador.model.Usuario;
 
+@WebServlet(urlPatterns ="/login")
 public class Login extends HttpServlet{
 	
 	private static final long serialVersionUID = -3796313427747926879L;
@@ -25,8 +28,13 @@ public class Login extends HttpServlet{
 		
 		Usuario usuario = new UsuarioDAO().buscaPorEmailESenha(email, senha);
 		
-		if (usuario.equals(null)) {
-			writer.append("<html><body>Usuário ou senha inválida</body></html>");
+		if (usuario == null) {
+			writer.println("<html><body>Usuário ou senha inválida</body></html>");
+		}else {
+			Cookie cookie = new Cookie("usuario", email);
+			writer.println("<html><body>Usuário " + email + " está logado!</body></html>");
+			
+			resp.addCookie(cookie);
 		}
 		
 	}
