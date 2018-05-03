b44e5c4b690ce7eb587308c256eb1a1fc1b832da
Marcos <marcos.goncalvesdossantos@gmail.com>
2018-05-03 18:04:18 -0300

    Cria Coolie

--- src/main/java/br/com/alura/gerenciador/web/Login.java
+++ src/main/java/br/com/alura/gerenciador/web/Login.java
@@ -13,29 +13,31 @@ import javax.servlet.http.HttpServletResponse;
 import br.com.alura.gerenciador.dao.UsuarioDAO;
 import br.com.alura.gerenciador.model.Usuario;
 
-@WebServlet(urlPatterns ="/login")
-public class Login extends HttpServlet{
-	
+@WebServlet(urlPatterns = "/login")
+public class Login extends HttpServlet {
+
 	private static final long serialVersionUID = -3796313427747926879L;
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter writer = resp.getWriter();
-		
+
 		String email = req.getParameter("email");
 		String senha = req.getParameter("senha");
-		
+
 		Usuario usuario = new UsuarioDAO().buscaPorEmailESenha(email, senha);
-		
+
 		if (usuario == null) {
 			writer.println("<html><body>Usuário ou senha inválida</body></html>");
-		}else {
+		} else {
 			Cookie cookie = new Cookie("usuario", email);
 			resp.addCookie(cookie);
-			
+
+			cookie.setMaxAge(10 * 60);
+
 			writer.println("<html><body>Usuário " + email + " está logado!</body></html>");
-			
+
 		}
-		
+
 	}
 }
---
+++ src/main/java/br/com/alura/gerenciador/web/Logout.java
@@ -0,0 +1,34 @@
+package br.com.alura.gerenciador.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/logout")
+public class Logout extends HttpServlet{
+
+	private static final long serialVersionUID = 7763927224856106937L;
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		Cookie cookie = getUsuario(req);
+		
+		if (cookie != null) {
+			cookie.setMaxAge(0);
+			resp.addCookie(cookie);
+		}
+		
+		PrintWriter writer = resp.getWriter();
+		
+		writer.println("<html><body>Usuário deslogado</body></html>");
+		
+	}
+	
+}
